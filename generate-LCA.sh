64297dc47e39f5baf7bba2516e8ff90c3b3b7a67
Rohan Maddamsetti <rohan.maddamsetti@gmail.com>
2019-10-11 16:46:21 -0400

    add GDparsing

--- generate-LCA.sh
+++ generate-LCA.sh
@@ -8,7 +8,7 @@ gdtools APPLY -o LCA.gff3 -f GFF3 -r ../REL606.7.gbk LCA.gd
 gdtools APPLY -o LCA.fasta -f FASTA -r ../REL606.7.gbk LCA.gd
 gdtools ANNOTATE -r ../REL606.7.gbk -o annotated_LCA.html -f HTML LCA.gd
 
-## horrible hacks to make a genbank file from the LCA ggf3 file
+## horrible hacks to make a genbank file from the LCA gff3 file
 ## for the dice analysis.
 python ../../src/external/gff_to_genbank.py LCA.gff3 LCA.fasta
 sed -i '' -e 's/Name=/gene=/g' ../genomes/curated-diffs/LCA.gbk
